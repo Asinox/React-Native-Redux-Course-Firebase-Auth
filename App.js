414bedf1d1ce76ec22adeb77b9c2d32c6e538830
Juan Miguel Calcaño <asinox@gmail.com>
2019-01-04 09:35:46 -0400

    removing token

--- App.js
+++ App.js
@@ -18,12 +18,7 @@ export default class App extends Component {
   componentWillMount(){
     firebase.initializeApp(
       {
-        apiKey: 'AIzaSyDo2BT6kwNCWEHn1Ik4Pq6PDQ6Lj5z0Ko0',
-        authDomain: 'auth-udemy-6cf5c.firebaseapp.com',
-        databaseURL: 'https://auth-udemy-6cf5c.firebaseio.com',
-        projectId: 'auth-udemy-6cf5c',
-        storageBucket: 'auth-udemy-6cf5c.appspot.com',
-        messagingSenderId: '884312596049'
+        <apiKEY>
       }
     );
   }
